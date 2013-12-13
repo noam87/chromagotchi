c1ba42831190554247d0299435474e1a9206d804
noam <clusterfoo@server.fake>
2013-12-13 16:14:24 -0500

    image mode saves to localstorage

--- client/main.js
+++ client/main.js
@@ -1,12 +1,11 @@
 var render = require("./view/out");
 var input = require("./view/in");
+var Model = require("./model");
+
+window.saveImage = Model.set;
 
 $(document).ready(function() {
-  var test = [
-   [{ uni: 0  }, { uni: 2 }, { uni: 1 }, { uni: 0 }, { uni: 3  }, { uni: 0 }],
-   [{ uni: 2  }, { uni: 3 }, { uni: 4 }, { uni: 0  }, { uni: 0 }, { uni: 1 }],
-   [{ uni: 3  }, { uni: 0 }, { uni: 2  }, { uni: 3 }, { uni: 0 }, { uni: 1 }],
-  ];
+  var test = Model.get();
 
   render(test);
   input();
