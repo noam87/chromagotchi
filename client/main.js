eb43d67ba8c2982cf2afc7b3525c4eb1f13e9f93
noam <clusterfoo@server.fake>
2013-12-13 15:22:24 -0500

    view/out renders blocks base on input matrix.

--- client/main.js
+++ client/main.js
@@ -1,3 +1,11 @@
-poo = require("./test");
+var render = require("./view/out");
 
-poo();
+$(document).ready(function() {
+  var test = [
+   [{ uni: 0  }, { uni: 2 }, { uni: 1 }, { uni: 0 }, { uni: 3  }, { uni: 0 }],
+   [{ uni: 2  }, { uni: 3 }, { uni: 4 }, { uni: 0  }, { uni: 0 }, { uni: 1 }],
+   [{ uni: 3  }, { uni: 0 }, { uni: 2  }, { uni: 3 }, { uni: 0 }, { uni: 1 }],
+  ];
+
+  render(test);
+});
--- client/view/out/index.js
+++ client/view/out/index.js
@@ -11,11 +11,48 @@
 // and outputs an HTML string of the form
 // 
 //  <div>
-//    <span>pixel.char</span>
+//    <span>pixel.uni</span>
 //    ...
-//    <span>pixel.char</span>
+//    <span>pixel.uni</span>
 //  </div>
 //
 // where each div is a pixel array of unicode chars.
+
+module.exports = render;
+function render(input) {
+  var unis = ["&#x2588;", "&#x2593;", "&#x2592;", "&#x2591;", " "];
+  var $canvas = $("#canvas");
+
+  createRows(input);
+  createPixels(input);
+
+
+  function createRows(input) {
+    $canvas.html("");
+    input.forEach(createRow);
+
+    function createRow(row) {
+      $canvas.append("<div></div>") ;
+    }
+  }
+
+  function createPixels(input)  {
+    input.forEach(renderPixels);
+
+    function renderPixels(row, index)  {
+      row.forEach(function(pixelObj) { 
+        var block = toBlock(pixelObj.uni);
+        var $pixel = $("<span></span>").html(block);
+        var $row = $( $canvas.find("div").get(index) );
+        $row.append($pixel);
+      });
+    }
+  }
+
+  function toBlock(uni) {
+    return unis[uni];
+  }
+}
+
 
 
